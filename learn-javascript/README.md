9794ebb905350c324e5478a15eb433ab8218c8e7
Krishna Chaitanya M <chaitanya.jun12@gmail.com>
2020-03-28 15:13:59 +0530

    Typeof operator in JS.

--- learn-javascript/README.md
+++ learn-javascript/README.md
@@ -3,4 +3,4 @@
 1. [Object values](https://github.com/chaitanyajun12/learn-it/blob/master/learn-javascript/javascript-101/object_values.js)
 2. [Primitive values](https://github.com/chaitanyajun12/learn-it/blob/master/learn-javascript/javascript-101/primitive_values.js)
 3. [String delimiters](https://github.com/chaitanyajun12/learn-it/blob/master/learn-javascript/javascript-101/string_delimiters.js)
-
+3. [Typeof operator](https://github.com/chaitanyajun12/learn-it/blob/master/learn-javascript/javascript-101/typeof_values.js)
---
+++ learn-javascript/javascript-101/typeof_values.js
@@ -0,0 +1,25 @@
+// typeof operator is used to find the type of the value.
+
+var name = "Krishna";
+console.log(typeof name); // string type
+
+var num = 100;
+console.log(typeof num); // number type
+
+var x = true;
+console.log(typeof x); // boolean type
+
+var y = null;
+console.log(typeof y); //object type :O
+
+var z = undefined;
+console.log(typeof z); // undefined type
+
+var arr = ['hello', 'world'];
+console.log(typeof arr); // object type
+
+var obj = { "name": "Krishna Chaitanya M" };
+console.log(typeof obj); // object type
+
+var func = function hello() {};
+console.log(typeof func); // function type
